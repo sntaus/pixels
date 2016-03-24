26f1ff2d2d09b27909ae7f3b526ea534d79722ef
Shehzad Noor Taus Priyo <sntaus@gmail.com>
2016-03-23 20:28:28 -0700

    Separated CSS files

--- client/app/services/API.js
+++ client/app/services/API.js
@@ -1,13 +1,13 @@
 angular.module("pixels").factory("API", ["$http", "baseUrl", "galleryEndpoint", "photoDetailsEndpoint", function ($http, baseUrl, galleryEndpoint, photoDetailsEndpoint) {
     return {
-        getGallery: function(success, failure) {
+        getGallery: function (success, failure) {
             $http({
                 method: "POST",
                 url: baseUrl + galleryEndpoint
             }).then(success, failure);
         },
 
-        getDetails: function(photoId, success, failure) {
+        getDetails: function (photoId, success, failure) {
             $http({
                 method: "POST",
                 url: baseUrl + photoDetailsEndpoint,
